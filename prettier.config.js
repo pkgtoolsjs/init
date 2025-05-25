144cf4220a66f045618a20774052fe445a527e91
Winter <doyouknowmeemail@gmail.com>
2025-05-25 19:36:27 +0800

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import('prettier').Config}
+ */
+export default {
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  singleAttributePerLine: true
+}
